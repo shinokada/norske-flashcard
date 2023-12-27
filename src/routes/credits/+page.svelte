c3ef77a19b4ffad665330f17245693aea1837b64
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2023-12-27 21:43:09 +0100

    fix: update credits page

--- src/routes/credits/+page.svelte
+++ src/routes/credits/+page.svelte
@@ -10,6 +10,15 @@
   <li class="hover:text-red-700 hover:underline">
     <a href="https://svelte-5-preview.vercel.app/docs/introduction" target="_blank" class="hover:underline me-4 md:me-6">Svelte 5</a>
   </li>
+  <li>
+    <a href="https://tailwindcss.com/" target="_blank" class="hover:underline me-4 md:me-6">TailwindCSS</a>
+  </li>
+  <li>
+    <a href="https://flowbite.com/" target="_blank" class="hover:underline me-4 md:me-6">Flowbite</a>
+  </li>
+  <li>
+    <a href="https://pptr.dev/" target="_blank" class="hover:underline me-4 md:me-6">Puppeteer for scraping</a>
+  </li>
   <li class="hover:text-red-700 hover:underline">
     <a href="https://github.com/shinokada/norsk-flashcard" target="_blank" class="hover:underline me-4 md:me-6">Github repo</a>
   </li>
